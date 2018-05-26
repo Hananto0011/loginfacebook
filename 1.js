1c654a804d0fd3dc71fe083bfa606cb6aab994c8
Hananto0011 <hananto.b@students.amikom.ac.id>
2018-05-26 10:27:21 +0700

    baru

---
+++ 1.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+        
+$("#tombollogin").click(function(){
+    alert("click");
+    $.ajax({
+    url: "https://reqres.in/api/login",
+    type: "POST",
+    data: {
+        email: $("#uname").val(),
+        password: $("#psw").val()
+    },
+    success: function(response){
+        alert(response);
+        console.log(response);
+    }
+});
+}); 
+});
+
+function onSuccess(googleUser) {
+      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    }
+    function onFailure(error) {
+      console.log(error);
+    }
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    }
+
